7e30d91f517dd0620255e0a1c622f30b3a8f1a28
Madhurmehare27 <117211050+MADHURMEHARE@users.noreply.github.com>
2025-10-17 01:34:42 +0530

    adding dynamic route student page

--- components/homepage/MembershipSection.tsx
+++ components/homepage/MembershipSection.tsx
@@ -1,6 +1,6 @@
 
  import { plans } from '@/components/homepage/utils/data/DATA';
- import { Card } from '../common/Card';
+//  import { Card } from '../common/Card';
 
 export default function MembershipSection() {
 
@@ -51,15 +51,6 @@ export default function MembershipSection() {
           ))}
         </div>
       </div>
-      <Card
-      icon= "DynamicIcon" // The icon prop can be a component or a string
-    title=" madhur " 
-    description="abdjfdanc" 
-    bgColor="nadhcdhc " 
-    iconColor=""
-    variant = 'feature' 
-
-      />
     </section>
   );
 }
